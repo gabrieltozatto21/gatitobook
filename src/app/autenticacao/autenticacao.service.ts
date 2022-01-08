e85bd05572ace331b1251421e3420fede6363625
gabrieltozatto21 <gabriel.tozatto@hotmail.com>
2022-01-08 00:12:10 -0300

    adicionando tokensppara verificação de usuario

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http'
+import { Observable, tap } from 'rxjs';
+import { UsuarioService } from './usuario/usuario.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutenticacaoService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private usuarioService: UsuarioService) { }
 
-  autenticar(user: string, pass: string): Observable<any>{
+  autenticar(user: string, pass: string): Observable<HttpResponse<any>> {
     return this.httpClient.post('http://localhost:3000/user/login', {
       userName: user,
       password: pass
-    })
+    },
+      { observe: 'response' }
+    ).pipe(
+      tap(res => {
+        const authToken = res.headers.get('x-access-token') ?? '';
+        this.usuarioService.salvaToken(authToken);
+      })
+    )
   }
 }
